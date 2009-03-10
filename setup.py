87139f235219f9514fc25ae820a8af56e80a0703
Eric Naeseth <enaeseth@gmail.com>
2009-03-10 02:11:09 -0500

    Adding a setup.py script.

---
+++ setup.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(
+    name='Python FP-Growth',
+    description='A pure-python implementation of the FP-growth algorithm.'
+    version='0.1',
+    author='Eric Naeseth',
+    author_email='enaeseth@gmail.com',
+    url='http://github.com/enaeseth/python-fp-growth/',
+    
+    py_modules=['fp_growth']
+)
