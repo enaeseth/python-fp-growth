27554449dbbec3f038b6f0053e8cb3cad67ee5d3
Eric Naeseth <enaeseth@gmail.com>
2009-03-10 15:51:45 -0500

    Make the fp_growth module executable in a useful default state.

--- fp_growth.py
+++ fp_growth.py
@@ -357,3 +357,23 @@ class FPNode(object):
             return "<%s (root)>" % type(self).__name__
         return "<%s %r (%r)>" % (type(self).__name__, self.item, self.count)
 
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    import csv
+    
+    p = OptionParser(usage='%prog data_file')
+    p.add_option('-s', '--minimum-support', dest='minsup', type='int',
+        help='Minimum itemset support (default: 2)')
+    p.set_defaults(minsup=2)
+    
+    options, args = p.parse_args()
+    if len(args) < 1:
+        p.error('must provide the path to a CSV file to read')
+    
+    f = open(args[0])
+    try:
+        for itemset in find_frequent_itemsets(csv.reader(f), options.minsup):
+            print '{' + ', '.join(itemset) + '}'
+    finally:
+        f.close()
