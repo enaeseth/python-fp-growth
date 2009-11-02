59a49eef49177c383ac6c02aacb393ad3a8c8e49
Eric Naeseth <enaeseth@gmail.com>
2009-11-02 10:16:24 -0600

    Updating the version number and author email address.

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from distutils.core import setup
 setup(
     name='Python FP-Growth',
     description='A pure-python implementation of the FP-growth algorithm.',
-    version='0.1',
+    version='0.1.1',
     author='Eric Naeseth',
-    author_email='enaeseth@gmail.com',
+    author_email='eric@naeseth.com',
     url='http://github.com/enaeseth/python-fp-growth/',
     
     py_modules=['fp_growth']
