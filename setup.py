49763de81738523910e82c7fa07bb65c5679c809
Eric Naeseth <eric@naeseth.com>
2016-01-07 17:47:15 -0800

    Bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name='fp-growth',
     description='A pure-python implementation of the FP-growth algorithm.',
-    version='0.1.2',
+    version='0.1.3',
     author='Eric Naeseth',
     author_email='eric@naeseth.com',
     url='http://github.com/enaeseth/python-fp-growth/',
